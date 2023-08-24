7b24c650885edc94e7a5b39d97dcef7eb4a6650f
Jason Parrott <fanatic+github@gmail.com>
2023-08-24 12:06:58 -0600

    WIP

--- 10_voraciouscodestorage/server.go
+++ 10_voraciouscodestorage/server.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 
 	proxyproto "github.com/pires/go-proxyproto"
@@ -16,6 +19,9 @@ type Server struct {
 	l      net.Listener
 	cancel context.CancelFunc
 	wg     sync.WaitGroup
+
+	storage      map[string][][]byte
+	storageMutex sync.RWMutex
 }
 
 func NewServer(ctx context.Context, port string) (*Server, error) {
@@ -32,7 +38,7 @@ func NewServer(ctx context.Context, port string) (*Server, error) {
 	l = &proxyproto.Listener{Listener: l}
 
 	log.Printf("10_voraciouscodestorage at=server.listening addr=%q\n", l.Addr().String())
-	s := &Server{Addr: l.Addr().String(), l: l, cancel: cancel}
+	s := &Server{Addr: l.Addr().String(), l: l, cancel: cancel, storage: map[string][][]byte{}}
 
 	go s.acceptLoop(ctx)
 
@@ -82,4 +88,169 @@ func (s *Server) handleConn(conn net.Conn) {
 	}()
 	defer conn.Close()
 
+	log.Printf("10_voraciouscodestorage at=start addr=%q\n", conn.RemoteAddr().String())
+
+	fmt.Fprintf(conn, "READY\n")
+
+	scanner := NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		log.Printf("10_voraciouscodestorage at=input line=%q\n", line)
+
+		// All commands must have at least one field, otherwise close connection
+		if len(fields) == 0 {
+			fmt.Fprintf(conn, "ERR illegal method:\n")
+			return
+		}
+
+		switch strings.ToUpper(fields[0]) {
+		case "HELP":
+			fmt.Fprintf(conn, "OK usage: HELP|GET|PUT|LIST\n")
+			fmt.Fprintf(conn, "READY\n")
+		case "LIST":
+			// if operand is not ascii, return error
+			if !isASCII(fields[1]) || fields[1][0] != '/' {
+				fmt.Fprintf(conn, "ERR illegal dir name\n")
+				continue
+			}
+			s.storageMutex.RLock()
+			files := listDir(s.storage, fields[1])
+			s.storageMutex.RUnlock()
+
+			sort.Strings(files)
+			fmt.Fprintf(conn, "OK %d\n", len(files))
+			for _, f := range files {
+				fmt.Fprintf(conn, "%s\n", f)
+			}
+			fmt.Fprintf(conn, "READY\n")
+		case "PUT":
+			if len(fields) != 3 {
+				fmt.Fprintf(conn, "ERR usage: PUT file length newline data\n")
+				continue
+			}
+			if !isASCII(fields[1]) || fields[1][0] != '/' {
+				fmt.Fprintf(conn, "ERR illegal file name\n")
+				continue
+			}
+			length, err := strconv.Atoi(fields[2])
+			if err != nil {
+				fmt.Fprintf(conn, "ERR illegal file length\n")
+				continue
+			}
+			if length < 0 {
+				fmt.Fprintf(conn, "ERR illegal file length\n")
+				continue
+			}
+
+			// Read file data
+			data := make([]byte, length)
+			fmt.Printf("Reading %d bytes\n", len(data))
+			n, err := scanner.ReadFull(data)
+			if err != nil || n != length {
+				fmt.Printf("err=%s n=%d\n", err, n)
+				fmt.Fprintf(conn, "ERR reading file data\n")
+				continue
+			}
+
+			// Store file data
+			s.storageMutex.Lock()
+			s.storage[fields[1]] = append(s.storage[fields[1]], data)
+			revision := len(s.storage[fields[1]])
+			s.storageMutex.Unlock()
+
+			fmt.Fprintf(conn, "OK r%d\n", revision)
+			fmt.Fprintf(conn, "READY\n")
+
+		case "GET":
+			if len(fields) != 2 {
+				fmt.Fprintf(conn, "ERR usage: GET file [revision]\n")
+				continue
+			}
+
+			// if operand is not ascii, return error
+			if !isASCII(fields[1]) || fields[1][0] != '/' {
+				fmt.Fprintf(conn, "ERR illegal file name\n")
+				continue
+			}
+
+			var revision int
+			if len(fields) == 3 {
+				r, err := strconv.Atoi(fields[2])
+				if err != nil {
+					fmt.Fprintf(conn, "ERR illegal revision\n")
+					continue
+				}
+				if r < 0 {
+					fmt.Fprintf(conn, "ERR illegal revision\n")
+					continue
+				}
+				revision = r
+			}
+
+			// if file does not exist, return error
+
+			s.storageMutex.RLock()
+			revisions, ok := s.storage[fields[1]]
+			s.storageMutex.RUnlock()
+
+			if !ok {
+				fmt.Fprintf(conn, "ERR file does not exist\n")
+				continue
+			}
+
+			// if revision is specified, return that revision
+			if len(fields) == 3 {
+				if revision > len(revisions) {
+					fmt.Fprintf(conn, "ERR revision does not exist\n")
+					continue
+				}
+			} else {
+				// otherwise return latest revision
+				revision = len(revisions)
+			}
+
+			data := revisions[revision-1]
+			fmt.Fprintf(conn, "OK %d\n", len(data))
+			fmt.Fprintf(conn, "%s", data)
+			fmt.Fprintf(conn, "READY\n")
+
+		default:
+			fmt.Fprintf(conn, "ERR illegal method: %s\n", fields[0])
+			return
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("10_voraciouscodestorage at=handleConn err=%q\n", err)
+	}
+
+	log.Printf("10_voraciouscodestorage at=finish addr=%q\n", conn.RemoteAddr().String())
+}
+
+func isASCII(s string) bool {
+	for _, r := range s {
+		if r > 127 {
+			return false
+		}
+	}
+	return true
+}
+
+func listDir(storage map[string][][]byte, dir string) []string {
+	if dir[len(dir)-1] != '/' {
+		dir += "/"
+	}
+	files := []string{}
+	for f := range storage {
+		if strings.HasPrefix(f, dir) {
+			baseName := strings.TrimPrefix(f, dir)
+			if strings.Contains(baseName, "/") {
+				dirName := strings.Split(baseName, "/")[0] + "/"
+				files = append(files, dirName+" DIR")
+			} else {
+				files = append(files, baseName+" r1")
+			}
+		}
+	}
+	return files
 }
--- tests/10_voraciouscodestorage_test.go
+++ tests/10_voraciouscodestorage_test.go
@@ -1,10 +1,14 @@
 package tests
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"net"
 	"testing"
 
 	voraciouscodestorage "github.com/fanatic/protohackers/10_voraciouscodestorage"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,4 +17,185 @@ func TestLevel10VoraciousCodeStorage(t *testing.T) {
 	s, err := voraciouscodestorage.NewServer(ctx, "")
 	require.NoError(t, err)
 	defer s.Close()
+
+	t.Run("bad method", func(t *testing.T) {
+		conn, err := net.Dial("tcp", s.Addr)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		scanner := bufio.NewScanner(conn)
+
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("\n"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "ERR illegal method:", scanner.Text())
+	})
+
+	t.Run("help", func(t *testing.T) {
+		conn, err := net.Dial("tcp", s.Addr)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		scanner := bufio.NewScanner(conn)
+
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("help\n"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "OK usage: HELP|GET|PUT|LIST", scanner.Text())
+
+		scanner.Scan()
+		assert.Equal(t, "READY", scanner.Text())
+	})
+
+	t.Run("list", func(t *testing.T) {
+		conn, err := net.Dial("tcp", s.Addr)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		scanner := bufio.NewScanner(conn)
+
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		t.Run("bad dir name", func(t *testing.T) {
+			_, err = conn.Write([]byte("list a\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "ERR illegal dir name", scanner.Text())
+		})
+
+		t.Run("empty dir", func(t *testing.T) {
+			_, err = conn.Write([]byte("list /\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "OK 0", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "READY", scanner.Text())
+		})
+
+		t.Run("non-empty dir", func(t *testing.T) {
+			_, err = conn.Write([]byte("put /a 0\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "OK r1", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "READY", scanner.Text())
+
+			_, err = conn.Write([]byte("put /b/c 0\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "OK r1", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "READY", scanner.Text())
+
+			_, err = conn.Write([]byte("list /\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "OK 2", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "a r1", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "b/ DIR", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "READY", scanner.Text())
+		})
+
+		t.Run("list subdirectory", func(t *testing.T) {
+			_, err = conn.Write([]byte("list /b\n"))
+			require.NoError(t, err)
+
+			scanner.Scan()
+			assert.Equal(t, "OK 1", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "c r1", scanner.Text())
+			scanner.Scan()
+			assert.Equal(t, "READY", scanner.Text())
+		})
+	})
+
+	t.Run("put", func(t *testing.T) {
+		conn, err := net.Dial("tcp", s.Addr)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		scanner := bufio.NewScanner(conn)
+
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("put a\n"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "ERR usage: PUT file length newline data", scanner.Text())
+
+		_, err = conn.Write([]byte("put /c 0\n"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "OK r1", scanner.Text())
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("put /c 1\n"))
+		require.NoError(t, err)
+		_, err = conn.Write([]byte("2"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "OK r2", scanner.Text())
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+	})
+
+	t.Run("get", func(t *testing.T) {
+		conn, err := net.Dial("tcp", s.Addr)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		scanner := bufio.NewScanner(conn)
+
+		scanner.Scan()
+		require.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("put /d 1\n1"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "OK r1", scanner.Text())
+		scanner.Scan()
+		assert.Equal(t, "READY", scanner.Text())
+
+		_, err = conn.Write([]byte("get\n"))
+		require.NoError(t, err)
+
+		fmt.Printf("Here\n")
+		scanner.Scan()
+		assert.Equal(t, "ERR usage: GET file [revision]", scanner.Text())
+		scanner.Scan()
+		assert.Equal(t, "READY", scanner.Text())
+		fmt.Printf("Here\n")
+
+		_, err = conn.Write([]byte("get /d\n"))
+		require.NoError(t, err)
+
+		scanner.Scan()
+		assert.Equal(t, "OK 1", scanner.Text())
+
+		scanner.Scan()
+		assert.Equal(t, "1READY", scanner.Text())
+	})
 }
